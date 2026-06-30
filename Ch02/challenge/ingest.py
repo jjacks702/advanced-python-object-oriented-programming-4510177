77d411f3db29063c1e838deb26927436d0146a4c
jjacks702 <jjacks074@gmail.com>
2026-06-30 14:55:35 -0700

    Update ingest.py

--- Ch02/challenge/ingest.py
+++ Ch02/challenge/ingest.py
@@ -19,8 +19,24 @@ class AccessEvent:
 # Add an ability to load events from JSON data (str)
 # Use a Mixin class
 # Make sure that Events have notify_loaded method (ABC)
+from abc import ABC, abstractMethod
+import json
+    
+    class Serialized(ABC):
+        @classmethod
+        def from_json(cls,data)
+        params = json.loads(data)
+        inst = cls(**params)
+        inst.notify_loaded()
+        return inst
+        
+    @classmethod
+    def notify_loaded(self):
+        pass
+#MIXIN
+class serialAccess(AccessEvent,Serialized)
 
-
+class serialLogin(LoginEvent,Serialized)
 # %% Test
 # LoginEvent
 login_data = '{"login": "elliot"}'
